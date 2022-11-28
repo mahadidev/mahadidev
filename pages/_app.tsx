7aa7bbb28cf1ddd4d9188dcd1898bbd7fd581d7f
mahadidev <mahadi.dev.pm@gmail.com>
2022-11-28 15:32:56 +0600

    Update Meta Tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,19 @@ export default function App({ Component, pageProps }: AppProps) {
 		<Provider store={store}>
 			<Head>
 				<title>Mahadi Dev | Full Stack Developer</title>
+				<meta
+					property="og:title"
+					content="Mahadi Hasan - Full Stack Programmer"
+				/>
+				<meta
+					property="og:description"
+					content="I am a young professionals passionate about web development and design. My mission is to help you grow your business"
+				/>
+				<meta property="og:url" content="https://mahadidev.vercel.app" />
+				<meta
+					property="og:image"
+					content="https://mahadidev.vercel.app/projects/kina_kata/thumbnail.png"
+				/>
 			</Head>
 			<header>
 				<Navigation />
