2c06ab7183daaa1ad906abacbbb83f91feeb2b04
mahadidev <mahadi.dev.pm@gmail.com>
2023-01-21 01:15:34 +0600

    fix reload issue

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
-const removeImports = require('next-remove-imports')();
 
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	compiler: {
-		styledComponents: true,
-	},
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
 };
 
-module.exports = removeImports({
-	experimental: { esmExternals: true },
-	reactStrictMode: true,
-	swcMinify: true,
-	compiler: {
-		styledComponents: true,
-	},
-});
+module.exports = nextConfig;
