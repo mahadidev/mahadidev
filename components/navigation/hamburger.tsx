b501637b71886d68f6160d41973f9f44fbca6eea
mahadidev <mahadi.dev.pm@gmail.com>
2023-01-31 15:32:34 +0600

    Update Resume

--- components/navigation/hamburger.tsx
+++ components/navigation/hamburger.tsx
@@ -1,87 +1,90 @@
-import { motion } from 'framer-motion';
-import React from 'react';
-import { Link } from 'react-scroll';
-import { RootState, setSidebar, useDispatch, useSelector } from '../../redux';
-import { Button, Container, Icon } from '../subComponents';
+import { motion } from "framer-motion";
+import React from "react";
+import { Link } from "react-scroll";
+import { RootState, setSidebar, useDispatch, useSelector } from "../../redux";
+import { Button, Container, Icon } from "../subComponents";
 import {
-	HamburgerButtons,
-	HamburgerIcon,
-	HamburgerWrapper,
-	Logo,
-	Menu,
-	MenuItem,
-} from './style';
+  HamburgerButtons,
+  HamburgerIcon,
+  HamburgerWrapper,
+  Logo,
+  Menu,
+  MenuItem,
+} from "./style";
+import * as NextLink from "next/link";
 
 const Hamburger = () => {
-	const siteState = useSelector((store: RootState) => store.site);
-	const themeState = useSelector((store: RootState) => store.theme);
-	const dispatch = useDispatch();
+  const siteState = useSelector((store: RootState) => store.site);
+  const themeState = useSelector((store: RootState) => store.theme);
+  const dispatch = useDispatch();
 
-	return (
-		<HamburgerWrapper>
-			<Container className="hamburger_container">
-				<Logo>
-					<Icon name="code" /> MAHADI DEV
-				</Logo>
-				<HamburgerIcon
-					onClick={() => {
-						dispatch(setSidebar(!siteState.isSidebar));
-					}}
-				>
-					<Icon name={siteState.isSidebar ? 'menuClose' : 'menu'} />
-				</HamburgerIcon>
-			</Container>
-			<motion.div
-				className="sidebar"
-				initial={{
-					transform: siteState.isSidebar
-						? 'translateX(0%)'
-						: 'translateX(100%)',
-				}}
-				animate={{
-					transform: siteState.isSidebar
-						? 'translateX(0%)'
-						: 'translateX(100%)',
-				}}
-			>
-				<Menu hamburger={true}>
-					{siteState.menu?.map(
-						(
-							item: {
-								name: string;
-								link: string;
-								icon: string;
-							},
-							i: number
-						) => (
-							<Link
-								key={i}
-								activeClass="active"
-								to={item.name}
-								spy={true}
-								smooth={true}
-								offset={-(siteState?.navHeight ? siteState?.navHeight : 100)}
-								duration={500}
-								onSetActive={() => {
-									dispatch(setSidebar(false));
-								}}
-							>
-								<MenuItem>{item.name}</MenuItem>
-							</Link>
-						)
-					)}
-				</Menu>
-				<HamburgerButtons>
-					<Button
-						bg={`rgb(${themeState.color.primary})`}
-						color={`rgb(${themeState.color.white})`}
-					>
-						Download CV
-					</Button>
-				</HamburgerButtons>
-			</motion.div>
-		</HamburgerWrapper>
-	);
+  return (
+    <HamburgerWrapper>
+      <Container className="hamburger_container">
+        <Logo>
+          <Icon name="code" /> MAHADI DEV
+        </Logo>
+        <HamburgerIcon
+          onClick={() => {
+            dispatch(setSidebar(!siteState.isSidebar));
+          }}
+        >
+          <Icon name={siteState.isSidebar ? "menuClose" : "menu"} />
+        </HamburgerIcon>
+      </Container>
+      <motion.div
+        className="sidebar"
+        initial={{
+          transform: siteState.isSidebar
+            ? "translateX(0%)"
+            : "translateX(100%)",
+        }}
+        animate={{
+          transform: siteState.isSidebar
+            ? "translateX(0%)"
+            : "translateX(100%)",
+        }}
+      >
+        <Menu hamburger={true}>
+          {siteState.menu?.map(
+            (
+              item: {
+                name: string;
+                link: string;
+                icon: string;
+              },
+              i: number
+            ) => (
+              <Link
+                key={i}
+                activeClass="active"
+                to={item.name}
+                spy={true}
+                smooth={true}
+                offset={-(siteState?.navHeight ? siteState?.navHeight : 100)}
+                duration={500}
+                onSetActive={() => {
+                  dispatch(setSidebar(false));
+                }}
+              >
+                <MenuItem>{item.name}</MenuItem>
+              </Link>
+            )
+          )}
+        </Menu>
+        <HamburgerButtons>
+          <NextLink.default href="./resume.pdf" target={"_blank"}>
+            <Button
+              bg={`rgb(${themeState.color.primary})`}
+              color={`rgb(${themeState.color.white})`}
+            >
+              Download RESUME
+            </Button>
+          </NextLink.default>
+        </HamburgerButtons>
+      </motion.div>
+    </HamburgerWrapper>
+  );
 };
 
 export default Hamburger;
--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -1,61 +1,64 @@
-import React, { useEffect, useRef } from 'react';
-import { Link } from 'react-scroll';
-import styled, { ThemeProvider } from 'styled-components';
-import { RootState, setNavHeight, useDispatch, useSelector } from '../../redux';
-import { Button, Container, Icon } from '../subComponents';
-import Hamburger from './hamburger';
-import { Logo, Menu, MenuItem, Nav } from './style';
+import React, { useEffect, useRef } from "react";
+import { Link } from "react-scroll";
+import styled, { ThemeProvider } from "styled-components";
+import { RootState, setNavHeight, useDispatch, useSelector } from "../../redux";
+import { Button, Container, Icon } from "../subComponents";
+import Hamburger from "./hamburger";
+import { Logo, Menu, MenuItem, Nav } from "./style";
+import * as NextLink from "next/link";
 
 const Navigation = () => {
-	const siteState = useSelector((store: RootState) => store.site);
-	const themeState = useSelector((store: RootState) => store.theme);
-	const theme = themeState;
+  const siteState = useSelector((store: RootState) => store.site);
+  const themeState = useSelector((store: RootState) => store.theme);
+  const theme = themeState;
 
-	const dispatch = useDispatch();
-	const navRef = useRef(null as null | HTMLDivElement);
-	useEffect(() => {
-		if (navRef.current) {
-			dispatch(setNavHeight(navRef.current?.clientHeight));
-		}
-	});
+  const dispatch = useDispatch();
+  const navRef = useRef(null as null | HTMLDivElement);
+  useEffect(() => {
+    if (navRef.current) {
+      dispatch(setNavHeight(navRef.current?.clientHeight));
+    }
+  });
 
-	return (
-		<ThemeProvider theme={theme}>
-			<Nav ref={navRef}>
-				<Container className="nav__container">
-					<Logo>
-						<Icon name="code" /> MAHADI DEV
-					</Logo>
-					<Menu>
-						{siteState.menu?.map(
-							(
-								item: {
-									name: string;
-									link: string;
-									icon: string;
-								},
-								i: number
-							) => (
-								<Link
-									key={i}
-									activeClass="active"
-									to={item.name}
-									spy={true}
-									smooth={true}
-									offset={-(siteState?.navHeight ? siteState?.navHeight : 100)}
-									duration={500}
-								>
-									<MenuItem>{item.name}</MenuItem>
-								</Link>
-							)
-						)}
-					</Menu>
-					<Button>DOWNLOAD CV</Button>
-				</Container>
-				<Hamburger />
-			</Nav>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <Nav ref={navRef}>
+        <Container className="nav__container">
+          <Logo>
+            <Icon name="code" /> MAHADI DEV
+          </Logo>
+          <Menu>
+            {siteState.menu?.map(
+              (
+                item: {
+                  name: string;
+                  link: string;
+                  icon: string;
+                },
+                i: number
+              ) => (
+                <Link
+                  key={i}
+                  activeClass="active"
+                  to={item.name}
+                  spy={true}
+                  smooth={true}
+                  offset={-(siteState?.navHeight ? siteState?.navHeight : 100)}
+                  duration={500}
+                >
+                  <MenuItem>{item.name}</MenuItem>
+                </Link>
+              )
+            )}
+          </Menu>
+          <NextLink.default href="./resume.pdf" target={"_blank"}>
+            <Button>DOWNLOAD RESUME</Button>
+          </NextLink.default>
+        </Container>
+        <Hamburger />
+      </Nav>
+    </ThemeProvider>
+  );
 };
 
 export default Navigation;
