10b190abd9b55d53fc2549eca5601648923b0a6b
mahadidev <mahadi.dev.pm@gmail.com>
2023-01-31 14:08:02 +0600

    Update Footer alignment

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,56 +1,57 @@
-import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import { RootState, useSelector } from '../../redux';
-import { Container, Social } from '../subComponents';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { RootState, useSelector } from "../../redux";
+import { Container, Social } from "../subComponents";
 
 const Footer = () => {
-	const themeState = useSelector((store: RootState) => store.theme);
+  const themeState = useSelector((store: RootState) => store.theme);
 
-	const Wrapper = styled('div')`
-		width: 100%;
-		padding: 1rem 0;
-		background: ${(props) => `rgb(${props.theme.color.purple})`};
-		color: ${(props) => props.theme.color.text};
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
+  const Wrapper = styled("div")`
+    width: 100%;
+    padding: 1rem 0;
+    background: ${(props) => `rgb(${props.theme.color.purple})`};
+    color: ${(props) => props.theme.color.text};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-		@media (max-width: 768px) {
-			flex-direction: column;
-		}
-	`;
+    @media (max-width: 768px) {
+      flex-direction: column;
+    }
+  `;
 
-	const CopyRightText = styled('p')`
-		width: 100%;
-		color: ${(props) => `rgb(${props.theme.color.white} / 80%)`};
+  const CopyRightText = styled("p")`
+    width: 100%;
+    max-width: max-content;
+    color: ${(props) => `rgb(${props.theme.color.white} / 80%)`};
 
-		@media (max-width: 768px) {
-			text-align: center;
-			margin-bottom: 1rem;
-		}
-	`;
+    @media (max-width: 768px) {
+      text-align: center;
+      margin-bottom: 1rem;
+    }
+  `;
 
-	const FooterSocial = styled(Social)`
-		@media (max-width: 768px) {
-			grid-template-columns: repeat(7, minmax(0, 1fr));
-		}
-	`;
+  const FooterSocial = styled(Social)`
+    @media (max-width: 768px) {
+      grid-template-columns: repeat(7, minmax(0, 1fr));
+    }
+  `;
 
-	return (
-		<ThemeProvider theme={themeState}>
-			<Wrapper>
-				<Container>
-					<CopyRightText>All Right reversed by Mahadi Hasan</CopyRightText>
-					<FooterSocial
-						bg={'none'}
-						padding={'0.5rem'}
-						color={`rgb(${themeState.color.white} / 80%)`}
-						text={false}
-					/>
-				</Container>
-			</Wrapper>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={themeState}>
+      <Wrapper>
+        <Container>
+          <CopyRightText>All Right reversed by Mahadi Hasan</CopyRightText>
+          <FooterSocial
+            bg={"none"}
+            padding={"0.5rem"}
+            color={`rgb(${themeState.color.white} / 80%)`}
+            text={false}
+          />
+        </Container>
+      </Wrapper>
+    </ThemeProvider>
+  );
 };
 
 export default Footer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,37 @@
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import { Footer, Navigation } from '../components';
-import { Provider, store } from '../redux';
-import '../styles/globals.css';
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Footer, Navigation } from "../components";
+import { Provider, store } from "../redux";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<Provider store={store}>
-			<Head>
-				<title>Mahadi Dev | Full Stack Developer</title>
-				<meta
-					property="og:title"
-					content="Mahadi Hasan - Full Stack Programmer"
-				/>
-				<meta
-					property="og:description"
-					content="I am a young professionals passionate about web development and design. My mission is to help you grow your business"
-				/>
-				<meta property="og:url" content="https://mahadidev.vercel.app" />
-				<meta
-					property="og:image"
-					content="https://mahadidev.vercel.app/projects/kina_kata/thumbnail.png"
-				/>
-			</Head>
-			<header>
-				<Navigation />
-			</header>
-			<main>
-				<Component {...pageProps} />
-			</main>
-			<footer>
-				<Footer />
-			</footer>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <Head>
+        <title>Mahadi Dev | Full Stack Developer</title>
+        <meta
+          property="og:title"
+          content="Mahadi Hasan - Full Stack Programmer"
+        />
+        <meta
+          property="og:description"
+          content="I am a young professionals passionate about web development and design. My mission is to help you grow your business"
+        />
+        <meta property="og:url" content="https://mahadidev.vercel.app" />
+        <meta
+          property="og:image"
+          content="https://mahadidev.vercel.app/projects/kina_kata/thumbnail.png"
+        />
+      </Head>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </Provider>
+  );
 }
